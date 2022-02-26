b90971a150511c007aca983d6270fd2ca3025a0c
Ralroid <86804406+Ralroid@users.noreply.github.com>
2022-02-25 21:36:19 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+student_array=[];
+
+function submit()
+{
+    var name_1 = document.getElementById("student_name_1").value;
+    var name_2 = document.getElementById("student_name_2").value;
+    var name_3 = document.getElementById("student_name_3").value;
+    var name_4 = document.getElementById("student_name_4").value;
+
+    student_array.push(name_1);
+    student_array.push(name_2);
+    student_array.push(name_3);
+    student_array.push(name_4);
+        
+        console.log(student_array);
+
+        document.getElementById("display_name").innerHTML=student_array;
+        document.getElementById("submit_button").style.display="none";
+        document.getElementById("sort_button").style.display="inline-block";
+    
+}
+
+function sorting()
+{
+    student_array.sort();
+    console.log(student_array);
+    document.getElementById("display_name").innerHTML=student_array;
+}
